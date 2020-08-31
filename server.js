4d2b8c874ac6553074b4662dfa9824a0e73fcca7
Aastha Gupta <aastha1406@gmail.com>
2020-08-31 13:59:28 +0530

    Connected to DB

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Welcome to the home page!");
 });
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server sarted on port ${PORT}`);
 });
