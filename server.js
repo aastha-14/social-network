09f72c8062919dedc7458382f5e2440cf62f7ed9
Aastha Gupta <aastha1406@gmail.com>
2020-08-31 13:35:28 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the home page!");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server sarted on port ${PORT}`);
+});
